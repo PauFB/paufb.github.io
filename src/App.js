c7430ebf78bd4822bede39e054333fd4f2fb13ef
Pau Fernandez Bardulet <paufernandezbardulet@gmail.com>
2024-07-16 12:32:48 +0200

    Make main content be pushed on sidebar display

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Home } from './components/pages/Home';
 import './App.css';
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  }
+
   return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+    <div className={`app-container ${isSidebarOpen ? "sidebar-open" : ""}`}>
+      <Navbar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   );
 }
