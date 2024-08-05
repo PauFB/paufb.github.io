fc432e0b45e5cfc396df1a83722110e97431998f
Pau Fernandez Bardulet <paufernandezbardulet@gmail.com>
2024-08-05 23:20:43 +0200

    Close navbar dropdown on item click

--- src/components/navbar/TopNavbarItem.js
+++ src/components/navbar/TopNavbarItem.js
@@ -3,27 +3,26 @@ import { NavLink } from 'react-router-dom';
 import { Dropdown } from './Dropdown';
 import './TopNavbarItem.css';
 
-export const TopNavbarItem = ({items, depthLevel}) => {
+export function TopNavbarItem({items, depthLevel}) {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   return (
-    <>
-      <li
-        onMouseEnter={() => setIsDropdownOpen(true)}
-        onMouseLeave={() => setIsDropdownOpen(false)}
-      >
-        {items.submenu ? (
-          <>
-            <NavLink to={items.url} onClick={(e) => {e.preventDefault()}}>
-              {items.title}
-              {depthLevel > 0 ? (<span className="arrow-right">&raquo;</span>) : (<span className="arrow-down" />)}
-            </NavLink>
-            <Dropdown submenus={items.submenu} depthLevel={depthLevel} isDropdownOpen={isDropdownOpen} />
-          </>
-        ) : (
-          <NavLink to={items.url}>{items.title}</NavLink>
-        )}
-      </li>
-    </>
+    <li
+      onMouseEnter={() => setIsDropdownOpen(true)}
+      onMouseLeave={() => setIsDropdownOpen(false)}
+      onClick={() => isDropdownOpen && setIsDropdownOpen(false)}
+    >
+      {items.submenu ? (
+        <>
+          <NavLink to={items.url} onClick={(e) => {e.preventDefault()}}>
+            {items.title}
+            {depthLevel > 0 ? (<span className="arrow-right">&raquo;</span>) : (<span className="arrow-down" />)}
+          </NavLink>
+          <Dropdown submenus={items.submenu} depthLevel={depthLevel} isDropdownOpen={isDropdownOpen} />
+        </>
+      ) : (
+        <NavLink to={items.url}>{items.title}</NavLink>
+      )}
+    </li>
   );
 }
