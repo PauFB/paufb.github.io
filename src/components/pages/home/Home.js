67dd3248a94a1921dffc1db1424643befab35806
Pau Fernandez Bardulet <58038009+PauFB@users.noreply.github.com>
2024-08-08 00:48:18 +0200

    Update Home page

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,13 +1,23 @@
+import { Link } from 'react-router-dom';
+import { ReactComponent as ReactLogo } from '../../../assets/react-logo.svg';
 import { ReactComponent as GithubLogo } from '../../../assets/github-logo.svg';
 import './Home.css';
 
-export const Home = () => {
+export function Home() {
   return (
-    <div className="container">
-      <a href="https://github.com/PauFB" target="_blank" rel="noreferrer">
-        <GithubLogo className="logo" />
-        @PauFB
-      </a>
+    <div className="home-page__container">
+      <Link to={"https://github.com/PauFB"} className="home-page__anchor" target="_blank" rel="noreferrer">
+        <div className="home-page__github-container">
+          <GithubLogo className="home-page__github-container__logo" />
+          @PauFB
+        </div>
+      </Link>
+      <Link to={"https://react.dev"} className="home-page__anchor" target="_blank" rel="noreferrer">
+        <div className="home-page__react-container">
+          <ReactLogo className="home-page__react-container__logo" />
+          18.3.1
+        </div>
+      </Link>
     </div>
   );
 }
