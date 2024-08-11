0815578b0dda8b395a8bc6bd8b348e85b884bc97
Pau Fernandez Bardulet <58038009+PauFB@users.noreply.github.com>
2024-08-11 20:28:00 +0200

    Navbar: Use BEM CSS naming

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,23 +10,23 @@ export function Navbar({ isSidebarOpen, toggleSidebar, isNarrow }) {
   const depthLevel = 0;
   return (
     <>
-      <div className="inline-container">
+      <div className="navbar-header">
         <HamburgerMenuIcon isSidebarOpen={isSidebarOpen} onClick={toggleSidebar} width="2em" height="2em" backgroundColor="#ffffff" />
-        <nav className="navbar-top" style={{display: isNarrow ? "none" : ""}}>
+        <nav className="navbar-top" style={{ display: isNarrow && "none" }}>
           <ul>
-            {navbarItemsDataWithFullUrls.map((item, index) =>
+            {navbarItemsDataWithFullUrls.map(item =>
               <TopNavbarItem items={item} depthLevel={depthLevel} key={item.url} />
             )}
           </ul>
         </nav>
-        <Link to={"https://github.com/PauFB"} target="_blank" rel="noreferrer" className={`navbar-github-logo ${isNarrow ? "centered" : ""}`}>
+        <Link to={"https://github.com/PauFB"} target="_blank" rel="noreferrer" className={`navbar-top__github-logo ${isNarrow ? "navbar-top__github-logo--centered" : ""}`}>
           <GithubLogo />
         </Link>
       </div>
-      <nav className={`navbar-left ${isSidebarOpen ? "open" : ""} ${isNarrow ? "narrow" : ""}`}>
+      <nav className={`navbar-left ${isSidebarOpen ? "navbar-left--open" : ""} ${isNarrow ? "navbar-left--narrow" : ""}`}>
         <ul>
-          {navbarItemsDataWithFullUrls.map((item, index) =>
-            <div className="navbar-left__item-container">
+          {navbarItemsDataWithFullUrls.map(item =>
+            <div className="navbar-left__item-container" key={item.url}>
               <LeftNavbarItem items={item} depthLevel={depthLevel} toggleSidebar={toggleSidebar} key={item.url} />
             </div>
           )}
