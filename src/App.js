e6782e55c639cfe92b5e2ca9a97b143330511e04
Pau Fernandez Bardulet <58038009+PauFB@users.noreply.github.com>
2024-09-09 21:52:37 +0200

    Update fonts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import './App.css';
+import './fonts.css'
 import { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Navbar } from './components/navbar/Navbar';
 import { WeaponsPage as FFVIIECWeapons } from './components/pages/games/final-fantasy-vii-ever-crisis/weapons/WeaponsPage';
 import { InventoryPage as FFXIIIInventoryPage } from './components/pages/games/final-fantasy-xiii/InventoryPage';
 import { HomePage } from './components/pages/home/HomePage';
-import './App.css';
 
 function App() {
   const [isViewportNarrow, setIsViewportNarrow] = useState(getIsViewportNarrow());
